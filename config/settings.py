38accd81aa7440aa1516e635ca35f5a61029a0f4
rezatn <rezatn0934@gmail.com>
2026-07-11 22:00:39 +0330

    :wrench: add installed app to setting INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django",
+    "rest_framework",
+    ###############################
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
