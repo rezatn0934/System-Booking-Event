dbb3c3a8b6210dc9d84208c6315aad521697f798
rezatn <rezatn0934@gmail.com>
2026-07-15 00:44:37 +0330

    :wrench: add swagger urls in debug mode only

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,35 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("api/v1/auth/", include("accounts.urls")),
     path("api/v1/", include("bookings.urls")),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += [
         path("admin/", admin.site.urls),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
     ]
