4a9f4865775ae1262ed6f069c085e7ba3b7240fd
rezatn <rezatn0934@gmail.com>
2026-07-14 20:13:24 +0330

    :hammer: add bookings app to settings INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     ###############################
     "accounts.apps.AccountsConfig",
+    "bookings.apps.BookingsConfig",
 ]
 
 MIDDLEWARE = [
