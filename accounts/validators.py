ff8ce30c623442b7bb1359121f44ac2432ed43cb
rezatn <rezatn0934@gmail.com>
2026-07-15 00:48:39 +0330

    :hammer: update phone_number_validator error message

--- accounts/validators.py
+++ accounts/validators.py
@@ -2,5 +2,5 @@ from django.core.validators import RegexValidator
 
 phone_number_validator = RegexValidator(
     regex=r"^09\d{9}$",
-    message="Phone number format is invalid.",
+    message="Phone number must start with '09' and contain exactly 11 digits.",
 )
